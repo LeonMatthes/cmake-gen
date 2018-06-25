143a95ac2f4d594a6443e514e503b51c841644f8
Leon Matthes <leon@matthes.biz>
2018-06-25 22:02:16 +0200

    Update main.js

--- bin/main.js
+++ bin/main.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 const process = require('process')
 const varName = process.argv[2]
-const pathName = process.argv[3]
+const pathName = process.argv[3] ? process.argv[3] : '.' 
 
 const Directory = require('../index.js')
 
-const dir = new Directory(pathName ? pathName : '.')
-dir.read()
+console.log('----------------- cmake-gen -----------------')
+console.log('Invoked in directory: ' + pathName + '\n')
+
+const dir = new Directory(pathName)
 dir.write(varName)
+
+console.log('\n----------------- cmake-gen -----------------')
+console.log('Finished successfully\n')
